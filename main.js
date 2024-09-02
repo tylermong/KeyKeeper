1fb5908cabd02d39d8c47f00dba262a0961c0ac0
Andres Carpio <andresgcarpio123@gmail.com>
2024-09-02 00:57:43 -0500

    Designed login page

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createMainWindow() {
     });
  
     const startUrl = url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'login.html'),
         protocol: 'file:',
     });
 
