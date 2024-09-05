c7f1928450ba5319926a0ca2eaa753ca375e3ccf
tylermong <117101211+tylermong@users.noreply.github.com>
2024-09-05 18:29:23 -0400

    Added buttons to access credentials in vault

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createMainWindow() {
     });
  
     const startUrl = url.format({
-        pathname: path.join(__dirname, 'settings.html'),
+        pathname: path.join(__dirname, 'vault.html'),
         protocol: 'file:',
     });
 
