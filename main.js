158fdc4ec1c196bbfc18ac6daca7f027dff59d66
tylermong <117101211+tylermong@users.noreply.github.com>
2024-09-07 01:20:48 -0400

    Rewrote the credentialManager to write new credentials to a JSON file

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const url = require('url');
 const path = require('path');
+const fs = require('fs');
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Main Window Title',
         width: 1280,
         height: 720,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
     });
- 
+
     const startUrl = url.format({
         pathname: path.join(__dirname, 'pages/html/vault.html'),
         protocol: 'file:',
+        slashes: true
     });
 
     mainWindow.loadURL(startUrl);
 }
 
-app.whenReady().then(createMainWindow); 
+app.whenReady().then(createMainWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow();
+    }
+});
---
+++ pages/js/credentialManager.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+const createCredential = document.getElementById('add-credential');
+const rightContainer = document.getElementById('right-container');
+
+createCredential.addEventListener('click', function() {
+    rightContainer.innerHTML =
+        `<form id="credential-form">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required><br><br>
+            
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            
+            <input type="submit" value="Submit">
+        </form>
+    `;
+
+    const form = document.getElementById('credential-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        const newCredential = {
+            name: name,
+            username: username,
+            password: password,
+        };
+
+        let credentials = [];
+        if (fs.existsSync('credentials.json')) {
+            credentials = JSON.parse(fs.readFileSync('credentials.json'));
+        }
+        credentials.push(newCredential);
+
+        fs.writeFileSync('credentials.json', JSON.stringify(credentials, null, 2));
+    });
+});
